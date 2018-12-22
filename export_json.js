64f1ce4665020d062993f231ee5d6ab2774a84ea
Jean Michel Dias <jeanmdias@jeanmdias.local>
2018-12-22 00:44:37 -0200

    MongoDB aggregate

--- export_json.js
+++ export_json.js
@@ -5,21 +5,48 @@ const assert = require('assert');
 
 const MongoClient = require('mongodb').MongoClient;
 
+console.log('**************************************************************************************');
+
 MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser: true}, function(err, client) {
    
-    const db = client.db('spike-layer-heatmap');
-    const collection = db.collection('airports');
+    const db = client.db('spike-flights-heatmap');
+    const col_airports = db.collection('airport');
+    const col_flightlg = db.collection('flights');
 
-    var document = [];
     var listLegs;
 
     async.series([
 
+        /*
         function(seriesCallback) {
-            collection.deleteMany({}, function(err, result) {
+            col_airports.createIndex( { 'iata_code': 1 }, function(err, result) {
                 assert.equal(null,err);
-                console.log('**************************************************************************************');
-                console.log('** '+String(result.deletedCount)+' records deleted.');
+                console.log('** Index created');
+                seriesCallback();
+            });
+        },
+
+        function(seriesCallback) {
+            col_flightlg.createIndex( { 'airportcode': 1 }, function(err, result) {
+                assert.equal(null,err);
+                console.log('** Index created');
+                seriesCallback();
+            });
+        },
+        */
+
+        function(seriesCallback) {
+            col_airports.deleteMany({}, function(err, result) {
+                assert.equal(null,err);
+                console.log('** '+String(result.deletedCount)+' records deleted at collection "airport"');
+                seriesCallback();
+            });
+        },
+
+        function(seriesCallback) {
+            col_flightlg.deleteMany({}, function(err, result) {
+                assert.equal(null,err);
+                console.log('** '+String(result.deletedCount)+' records deleted at collection "flights"');
                 seriesCallback();
             });
         },
@@ -29,7 +56,7 @@ MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser: true}, functio
             .pipe(csv())
             .on('data', function(data){
                 try {
-                    collection.insertOne(data, function(err, result) {
+                    col_airports.insertOne(data, function(err, result) {
                         assert.equal(err, null);
                     });
                 }
@@ -57,34 +84,42 @@ MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser: true}, functio
 
                     // Departure
                     function(callback) {
-                        collection.findOne({'iata_code':item.departureairportcode}, function(err, res) {
+                        col_airports.findOne({'iata_code':item.departureairportcode}, function(err, res) {
                             assert.equal(null, err);
                             if (!res) {
                                 console.log('Aeroporto '+item.departureairportcode+' sem coordenadas');
                                 callback();
                             } else {
-                                document.push({
+                                var document = {
+                                    airportcode: item.departureairportcode,
                                     lat: res.latitude_deg,
                                     lng: res.longitude_deg
+                                }
+                                col_flightlg.insertOne(document, function(err, result) {
+                                    assert.equal(err, null);
+                                    callback();
                                 });
-                                callback();
                             }
                         });
                     },
 
                     // Arrival
                     function(callback) {
-                        collection.findOne({'iata_code':item.arrivalairportcode}, function(err, res) {
+                        col_airports.findOne({'iata_code':item.arrivalairportcode}, function(err, res) {
                             assert.equal(null, err);
                             if (!res) {
                                 console.log('Aeroporto '+item.arrivalairportcode+' sem coordenadas');
                                 callback();
                             } else {
-                                document.push({
+                                var document = {
+                                    airportcode: item.arrivalairportcode,
                                     lat: res.latitude_deg,
                                     lng: res.longitude_deg
+                                }
+                                col_flightlg.insertOne(document, function(err, result) {
+                                    assert.equal(err, null);
+                                    callback();
                                 });
-                                callback();
                             }
                         }); 
                     }
@@ -98,18 +133,77 @@ MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser: true}, functio
                 seriesCallback();
             });         
         },
-        
+
         function(seriesCallback) {
-            fs.writeFile('public/points.json', '{"locations":'+JSON.stringify(document)+'}');
-            console.log('** JSON created successfully.');
-            seriesCallback();
-        },
+
+            var export_points = '{"locations":[';
+            var format_points = false;
+
+            col_airports.aggregate([
+                {
+                    $lookup: 
+                    {   
+                        from: "flights", 
+                        localField: "iata_code", 
+                        foreignField: "airportcode", 
+                        as: "legs"
+                    }
+                },
+                {
+                    $project:
+                    {
+                        _id:0,
+                        iata_code: "$iata_code",
+                        latitude_deg: "$latitude_deg",
+                        longitude_deg: "$longitude_deg",
+                        numOfPoints: { $size: "$legs" }
+                    }
+                },
+                { 
+                    $match: 
+                    { 
+                        numOfPoints: { $gt: 0 } 
+                    } 
+                }
+            ]).toArray(function(err, points) {
+                
+                //console.log(points);
+                async.forEach(points, function(point, eachCallback) {
+
+                    async.series([
+
+                        function(callback) {
+                            if(format_points)
+                            {
+                                export_points = export_points + ',';
+                            }
+                            export_points = export_points + '{"lat":"'+point.latitude_deg+'","lng":"'+point.longitude_deg+'", "weight":"'+point.numOfPoints+'"}';
+                            
+                            format_points = true;
+                            callback();
+                        }
+
+                    ], function(err, results) {
+                        eachCallback();
+                    }); 
+                            
+
+                }, function() {
+                    fs.writeFile('public/points.json', export_points+']}');
+                    console.log('** Points list exported');
+                    seriesCallback();
+                });   
+
+            });
+
+        }
 
     ], function() {
+
         client.close(function(err) {
           console.log('** MongoDB disconnected!');
           console.log('**************************************************************************************');
         });
+        
     });
-
 });
